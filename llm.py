00fbae716abc8e543c0f844225cf8f057fc48b90
Shadman Ali <shadman.ali@live.co.uk>
2024-02-08 23:56:13 +0530

    update prompt in llm.py

--- llm.py
+++ llm.py
@@ -8,6 +8,11 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 
-llm = ChatGoogleGenerativeAI(model="gemini-pro",google_api_key=GOOGLE_API_KEY)
-result = llm.invoke("who is shadman ?")
+llm = ChatGoogleGenerativeAI(
+    model="gemini-pro",
+    google_api_key=GOOGLE_API_KEY,
+    verbose=True,
+    temperature=0.7,
+)
+result = llm.invoke("Hello, how aare you?")
 print(result)
