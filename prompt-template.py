7eca0874c8da6d93dfc925b6e9360e3fcfbba027
Shadman Ali <shadman.ali@live.co.uk>
2024-02-09 00:18:28 +0530

    add system message and human message template tp prompt template

--- prompt-template.py
+++ prompt-template.py
@@ -5,7 +5,8 @@ load_dotenv()
 
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.prompts import ChatPromptTemplate
-
+from langchain.prompts import HumanMessagePromptTemplate
+from langchain_core.messages import HumanMessage, SystemMessage
 
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 
@@ -15,13 +16,23 @@ llm = ChatGoogleGenerativeAI(
     google_api_key=GOOGLE_API_KEY,
     verbose=True,
     temperature=0.7,
+    convert_system_message_to_human=True
 )
 
 # Define a prompt template
-prompt = ChatPromptTemplate.from_template("Tell me a joke about {subject}")
+prompt = ChatPromptTemplate.from_messages(
+     [
+        SystemMessage(
+            content=(
+                "Generate a list of 10 synonyms for the following word. Return the results as a comma seperated list."
+            )
+        ),
+        HumanMessagePromptTemplate.from_template("{input}"),
+    ]
+)
 
 # Create LLM chain
 chain = prompt | llm
 
-response = chain.invoke({"subject": "cat"})
+response = chain.invoke({"input": "happy"})
 print(response)
